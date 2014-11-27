53882b95fa27e1758bf64f1b2025d29c2717cba7
rayyrayy <ray61503@hotmail.com>
2014-11-27 18:31:05 +0800

    del function is almost finish
    Signed-off-by: rayyrayy <ray61503@hotmail.com>

---
+++ flashaccounting/easy money manager/src/com/example/easymoneymanager/delclass.java
@@ -0,0 +1,14 @@
+package com.example.easymoneymanager;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class delclass extends Activity{
+	
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.delclasspg);
+		
+	}
+
+}
